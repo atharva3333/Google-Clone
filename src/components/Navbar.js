3b087434a302689296fdedf276e1263c1af501ad
Atharva Deshpande <73531009+atharva3333@users.noreply.github.com>
2022-10-06 10:59:06 +0530

    Update Navbar.js
    Name changed

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -8,7 +8,7 @@ const Navbar = ({darkTheme,setDarkTheme}) => {
         <div className='flex justify-between items-center space-x-5 w-screen'>
              <Link to="/">
                <p className='text-4xl bg-blue-500 font-bold text-white py-1 px-2 rounded dark:bg-gray-500 dark:text-gray-900 '>
-                Google Clone
+                Search Engine
                </p>
              </Link>
              <button type='button' onClick={() => setDarkTheme(!darkTheme)} className='text-xl dark:bg-gray-900 bg-white border rounded-full px-2 py-1 hover:shadow-lg'>
